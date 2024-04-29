327ba80909ad9edc9dc8e021fc6340d71c428572
Gustavo H <gustavo.hurtado@usach.cl>
2024-04-28 22:45:47 -0400

    ADD: footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,14 +2,17 @@ export default function Footer() {
   return (
     <footer className="mt-auto text-center py-5">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <p className="text-sm text-gray-400">
-          Cover template for{' '}
-          <a
-            className="text-white decoration-2 underline underline-offset-2 font-medium hover:text-gray-200 hover:decoration-gray-400"
-            href="../index.html"
-          >
-            Preline
-          </a>
+        <p className="text-sm text-gray-400 flex flex-col">
+          <span>
+            Desarrollado con React + Tailwind + Typescript.{' '}
+            <a
+              className="text-white decoration-2 underline underline-offset-2 font-medium hover:text-gray-200 hover:decoration-gray-400"
+              href="https://github.com/ghurtadoarevalo/portfolio"
+            >
+              Ver proyecto
+            </a>
+          </span>
+          <p className="mt-2">© 2024 gHurtado_dev. </p>
         </p>
       </div>
     </footer>
